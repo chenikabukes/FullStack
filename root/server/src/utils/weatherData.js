6564190f62b0bc5830ce973d50b0218dde4e3321
Chenika Bukes <chenikacatherin@gmail.com>
2023-10-05 16:59:43 -0500

    fullstack: fixed .env file

--- root/server/src/utils/weatherData.js
+++ root/server/src/utils/weatherData.js
@@ -1,30 +1,3 @@
-// const constants = require("./config");
-
-// const weatherData = async (address) => {
-//   const url =
-//     constants.openWeatherMap.BASE_URL +
-//     encodeURIComponent(address) +
-//     `&appid=` +
-//     constants.openWeatherMap.PRIVATE_KEY;
-
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-
-//     if (response.status !== 200) {
-//       throw new Error("Can't fetch the data");
-//     }
-
-//     return {
-//       temperature: data.main.temp,
-//       description: data.weather[0].description,
-//       cityName: data.name,
-//       humidity: data.main.humidity,
-//     };
-//   } catch (error) {
-//     throw error;
-//   }
-// };
 require("dotenv").config();
 
 const weatherData = async (address) => {
@@ -32,7 +5,7 @@ const weatherData = async (address) => {
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/` +
     encodeURIComponent(address) +
     `?key=` +
-    PRIVATE_KEY;
+    process.env.PRIVATE_KEY;
 
   try {
     const response = await fetch(url);
